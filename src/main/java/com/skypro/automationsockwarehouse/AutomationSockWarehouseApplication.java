c6c4c0fa3ae081745dcd6e61c3aaab61f97d1b0f
evgen <89818121788@mail.ru>
2023-05-04 00:40:09 +0300

    First start

---
+++ src/main/java/com/skypro/automationsockwarehouse/AutomationSockWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.automationsockwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationSockWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationSockWarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/automationsockwarehouse/AutomationSockWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.automationsockwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationSockWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
